af87ea7ce0a864ca5210a524c96dfa0d8cd43d95
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-05-29 17:26:01 +0300

    feat(pages/login): add login form handling

--- src/pages/login/ui/login-page/LoginPage.tsx
+++ src/pages/login/ui/login-page/LoginPage.tsx
@@ -1,4 +1,14 @@
+import { useMutation } from '@tanstack/react-query';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { object, string } from 'yup';
+import { sessionModel } from '~entities/session';
+import { conduitApi } from '~shared/api';
+
 export function LoginPage() {
+  const login = useMutation((userData: conduitApi.LoginData) =>
+    conduitApi.Auth.login(userData),
+  );
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -9,28 +19,52 @@ export function LoginPage() {
               <a href="/#">Need an account?</a>
             </p>
 
-            <form>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="Email"
-                />
-              </fieldset>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="password"
-                  placeholder="Password"
-                />
-              </fieldset>
-              <button
-                className="btn btn-lg btn-primary pull-xs-right"
-                type="button"
-              >
-                Sign in
-              </button>
-            </form>
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+              }}
+              // TODO: add correct validation
+              validationSchema={object().shape({
+                email: string().required('requared'),
+                password: string().required('requared'),
+              })}
+              // TODO: handle server errors
+              onSubmit={async (values) => {
+                const data = await login.mutateAsync({ user: values });
+                sessionModel.sessionStore.getState().addToken(data.user.token);
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <fieldset className="form-group">
+                    <Field
+                      name="email"
+                      className="form-control form-control-lg"
+                      type="text"
+                      placeholder="Email"
+                    />
+                    <ErrorMessage name="email" />
+                  </fieldset>
+                  <fieldset className="form-group">
+                    <Field
+                      name="password"
+                      className="form-control form-control-lg"
+                      type="password"
+                      placeholder="Password"
+                    />
+                    <ErrorMessage name="password" />
+                  </fieldset>
+                  <button
+                    className="btn btn-lg btn-primary pull-xs-right"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Sign in
+                  </button>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
