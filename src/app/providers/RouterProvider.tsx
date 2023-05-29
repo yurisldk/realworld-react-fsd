f48cd66d7a3f1418fa252f878b25a1c600a6bf97
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-05-29 17:22:23 +0300

    feat(app/providers): add guest guard to router

--- src/app/providers/RouterProvider.tsx
+++ src/app/providers/RouterProvider.tsx
@@ -1,8 +1,23 @@
 import { Navigate, useRoutes } from 'react-router-dom';
+import { useStore } from 'zustand';
+import { sessionModel } from '~entities/session';
 import { HomePage } from '~pages/home';
 import { MainLayout } from '~pages/layouts';
 import { LoginPage } from '~pages/login';
 import { RegisterPage } from '~pages/register';
+
+type GuestGuardProps = {
+  children: React.ReactNode;
+};
+
+function GuestGuard(props: GuestGuardProps) {
+  const { children } = props;
+  const token = useStore(sessionModel.sessionStore, (state) => state.token);
+
+  if (token) return <Navigate to="/" />;
+
+  return <> {children} </>;
+}
 
 export function Router() {
   return useRoutes([
@@ -10,8 +25,22 @@ export function Router() {
       element: <MainLayout />,
       children: [
         { element: <HomePage />, index: true },
-        { path: 'login', element: <LoginPage /> },
-        { path: 'register', element: <RegisterPage /> },
+        {
+          path: 'login',
+          element: (
+            <GuestGuard>
+              <LoginPage />
+            </GuestGuard>
+          ),
+        },
+        {
+          path: 'register',
+          element: (
+            <GuestGuard>
+              <RegisterPage />
+            </GuestGuard>
+          ),
+        },
       ],
     },
     { path: '*', element: <Navigate to="/404" replace /> },
--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,8 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
+import { useStore } from 'zustand';
+import { sessionModel } from '~entities/session';
+import { setToken } from '~shared/api/conduitApi';
 import { QueryClientProvider } from './QueryClientProvider';
 import { Router } from './RouterProvider';
 
 export function Provider() {
+  const token = useStore(sessionModel.sessionStore, (state) => state.token);
+  if (token) setToken(token);
+
   return (
     <QueryClientProvider>
       <BrowserRouter>
