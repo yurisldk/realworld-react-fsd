c0a8013ad9c1c1f77f0f3ef0ecd1ff5082ab5dcd
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-05-30 13:45:57 +0300

    feat(pages/layoyt): add auth condition to main layout

--- src/pages/layouts/main-layout/ui/main-layout/MainLayout.tsx
+++ src/pages/layouts/main-layout/ui/main-layout/MainLayout.tsx
@@ -1,6 +1,27 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import { sessionApi, sessionModel } from '~entities/session';
+
+function CurrentUserPreview() {
+  const { data: userData, isLoading, isError } = sessionApi.useCurrentUser();
+
+  // TODO: Add error handle
+  if (isError) return <div>error</div>;
+  // TODO: Add fallback
+  if (isLoading) return <div>loading</div>;
+
+  const { image, username } = userData.user;
+
+  return (
+    <>
+      <img className="user-pic" src={image} alt={username} />
+      {username}
+    </>
+  );
+}
 
 export function MainLayout() {
+  const isAuth = sessionModel.useAuth();
+
   return (
     <>
       <nav className="navbar navbar-light">
@@ -8,38 +29,56 @@ export function MainLayout() {
           <NavLink className="navbar-brand" to="/">
             conduit
           </NavLink>
-          <ul className="nav navbar-nav pull-xs-right">
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-                to="/login"
-              >
-                Sign in
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-                to="/register"
-              >
-                Sign up
-              </NavLink>
-            </li>
-          </ul>
+          {!isAuth && (
+            <ul className="nav navbar-nav pull-xs-right">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Sign in
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register">
+                  Sign up
+                </NavLink>
+              </li>
+            </ul>
+          )}
+          {isAuth && (
+            <ul className="nav navbar-nav pull-xs-right">
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/qwerty">
+                  <i className="ion-compose" />
+                  &nbsp;New Article
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/qwerty">
+                  <i className="ion-gear-a" />
+                  &nbsp;Settings
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/qwerty">
+                  <CurrentUserPreview />
+                </NavLink>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
       <Outlet />;
