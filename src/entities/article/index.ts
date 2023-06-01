f5e54aec86163fccec7fca35849f41686d76827d
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-01 17:45:59 +0300

    feat(entities/article): add article filter model

--- src/entities/article/index.ts
+++ src/entities/article/index.ts
@@ -1,5 +1,7 @@
 export * as articleApi from './api/articleApi';
 
+export * as articleFilterModel from './model/articleFilterModel';
+
 export { ArticlePreviewCard } from './ui/article-preview-card/ArticlePreviewCard';
 export { ArticlesList } from './ui/articles-list/ArticlesList';
 export { LoadMoreButton } from './ui/load-more-button/LoadMoreButton';
---
+++ src/entities/article/model/articleFilterModel.ts
@@ -0,0 +1,51 @@
+import { StateCreator, StoreApi, useStore } from 'zustand';
+// TODO: remove these
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { devtools } from 'zustand/middleware';
+
+export type ArticleFilter = {
+  userfeed?: boolean;
+  global?: boolean;
+  tag?: string;
+  author?: string;
+  favorited?: string;
+  offset?: number;
+  limit?: number;
+};
+
+export type ArticleFilterState = {
+  filter: ArticleFilter;
+  setFilter: (filter: ArticleFilter) => void;
+};
+
+const initialFilterState: ArticleFilter = {
+  offset: 0,
+  limit: 10,
+};
+
+export const createArticleFilterSlice: StateCreator<
+  ArticleFilterState,
+  [['zustand/devtools', never]],
+  [],
+  ArticleFilterState
+> = (set) => ({
+  filter: initialFilterState,
+
+  setFilter: (filter: ArticleFilter) =>
+    set(
+      () => ({
+        filter: {
+          ...initialFilterState,
+          ...filter,
+        },
+      }),
+      false,
+      'articleFilter/setFilter',
+    ),
+});
+
+export function selectFilter(model: StoreApi<ArticleFilterState>) {
+  return useStore(model, (state) => state.filter);
+}
