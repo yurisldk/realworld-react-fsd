62c225dda050b82aeeb9e90bfa37c3f6e7623f51
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-01 15:00:07 +0300

    refactor(pages/profile): update feed and profile logic

--- src/pages/profile/ui/profile-page/ProfilePage.tsx
+++ src/pages/profile/ui/profile-page/ProfilePage.tsx
@@ -1,34 +1,85 @@
+import { useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+import { profileApi } from '~entities/profile';
+import { FavoritedArticlesList } from '~widgets/favorited-articles-list';
+import { ProfileArticlesList } from '~widgets/profile-articles-list';
+
+type TabsState = {
+  profile: boolean;
+  favorites: boolean;
+};
+
+type Action = { type: 'profile' } | { type: 'favorites' };
+
+function reducer(_: TabsState, action: Action): TabsState {
+  switch (action.type) {
+    case 'profile':
+      return { profile: true, favorites: false };
+    case 'favorites':
+      return { profile: false, favorites: true };
+
+    default:
+      throw new Error();
+  }
+}
+
+function toggleProfile(dispatch: React.Dispatch<Action>) {
+  return dispatch({ type: 'profile' });
+}
+
+function toggleFavorites(dispatch: React.Dispatch<Action>) {
+  return dispatch({ type: 'favorites' });
+}
+
 type ProfilePageProps = {
   favorites?: boolean;
 };
 
 export function ProfilePage(props: ProfilePageProps) {
   const { favorites } = props;
+  const { username } = useParams();
+
+  // TODO: handle error
+  // TODO: navigate to 404 username that doesnt exists (404)
+  const {
+    data: profile,
+    isLoading,
+    isError,
+    isSuccess,
+  } = profileApi.useProfile(username!);
+
+  const initialState: TabsState = {
+    profile: !favorites,
+    favorites: Boolean(favorites),
+  };
+
+  const [tabs, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="profile-page">
       <div className="user-info">
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-md-10 offset-md-1">
-              <img
-                src="http://i.imgur.com/Qr71crq.jpg"
-                className="user-img"
-                alt="qwert"
-              />
-              <h4>Eric Simons</h4>
-              <p>
-                Cofounder @GoThinkster, lived in Aol&apos;s HQ for a few months,
-                kinda looks like Peeta from the Hunger Games
-              </p>
-              <button
-                className="btn btn-sm btn-outline-secondary action-btn"
-                type="button"
-              >
-                <i className="ion-plus-round" />
-                &nbsp; Follow Eric Simons
-              </button>
-            </div>
+            {isLoading && <div>loading</div>}
+            {isError && <div>error</div>}
+            {isSuccess && (
+              <div className="col-xs-12 col-md-10 offset-md-1">
+                <img
+                  src={profile.image}
+                  className="user-img"
+                  alt={profile.username}
+                />
+                <h4>{profile.username}</h4>
+                <p>{profile.bio}</p>
+                <button
+                  className="btn btn-sm btn-outline-secondary action-btn"
+                  type="button"
+                >
+                  <i className="ion-plus-round" />
+                  &nbsp; Follow {profile.username}
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -40,74 +91,33 @@ export function ProfilePage(props: ProfilePageProps) {
               <ul className="nav nav-pills outline-active">
                 <li className="nav-item">
                   {/* FIXME: add classnames */}
-                  <a className={`nav-link ${!favorites && 'active'}`} href="/">
+                  <button
+                    className={`nav-link ${tabs.profile && 'active'}`}
+                    type="button"
+                    onClick={() => toggleProfile(dispatch)}
+                  >
                     My Articles
-                  </a>
+                  </button>
                 </li>
                 <li className="nav-item">
-                  <a className={`nav-link ${favorites && 'active'}`} href="/">
+                  <button
+                    className={`nav-link ${tabs.favorites && 'active'}`}
+                    type="button"
+                    onClick={() => toggleFavorites(dispatch)}
+                  >
                     Favorited Articles
-                  </a>
+                  </button>
                 </li>
               </ul>
             </div>
 
-            <div className="article-preview">
-              <div className="article-meta">
-                <a href="/">
-                  <img src="http://i.imgur.com/Qr71crq.jpg" alt="qwert" />
-                </a>
-                <div className="info">
-                  <a href="/" className="author">
-                    Eric Simons
-                  </a>
-                  <span className="date">January 20th</span>
-                </div>
-                <button
-                  className="btn btn-outline-primary btn-sm pull-xs-right"
-                  type="button"
-                >
-                  <i className="ion-heart" /> 29
-                </button>
-              </div>
-              <a href="/" className="preview-link">
-                <h1>How to build webapps that scale</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
-            </div>
+            {tabs.profile && (
+              <ProfileArticlesList username={username as string} />
+            )}
 
-            <div className="article-preview">
-              <div className="article-meta">
-                <a href="/">
-                  <img src="http://i.imgur.com/N4VcUeJ.jpg" alt="qwert" />
-                </a>
-                <div className="info">
-                  <a href="/" className="author">
-                    Albert Pai
-                  </a>
-                  <span className="date">January 20th</span>
-                </div>
-                <button
-                  className="btn btn-outline-primary btn-sm pull-xs-right"
-                  type="button"
-                >
-                  <i className="ion-heart" /> 32
-                </button>
-              </div>
-              <a href="/" className="preview-link">
-                <h1>
-                  The song you won&apos;t ever stop singing. No matter how hard
-                  you try.
-                </h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-                <ul className="tag-list">
-                  <li className="tag-default tag-pill tag-outline">Music</li>
-                  <li className="tag-default tag-pill tag-outline">Song</li>
-                </ul>
-              </a>
-            </div>
+            {tabs.favorites && (
+              <FavoritedArticlesList username={username as string} />
+            )}
           </div>
         </div>
       </div>
