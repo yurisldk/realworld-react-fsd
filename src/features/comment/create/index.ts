21f07c4ee625893752556d3788a7838cfa5221c1
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-06 14:52:12 +0300

    feat(features/comment): add create comment feature

---
+++ src/features/comment/create/index.ts
@@ -0,0 +1,1 @@
+export { useCreateComment } from './model/createComment';
---
+++ src/features/comment/create/model/createComment.ts
@@ -0,0 +1,74 @@
+import { QueryClient, useMutation } from '@tanstack/react-query';
+import {
+  CommentDto,
+  GenericErrorModelDto,
+  HttpResponse,
+  NewCommentDto,
+  ProfileDto,
+  UserDto,
+  realworldApi,
+} from '~shared/api/realworld';
+
+type UseCreateCommentProps = {
+  slug: string;
+  comment: NewCommentDto;
+};
+
+export function useCreateComment(
+  queryKey: unknown[],
+  queryClient: QueryClient,
+) {
+  return useMutation<
+    CommentDto,
+    HttpResponse<unknown, GenericErrorModelDto>,
+    UseCreateCommentProps,
+    { prevComments: CommentDto[] }
+  >(
+    async ({ slug, comment }: UseCreateCommentProps) => {
+      const response = await realworldApi.articles.createArticleComment(slug, {
+        comment,
+      });
+
+      return response.data.comment;
+    },
+    {
+      onMutate: async ({ slug, comment }) => {
+        await queryClient.cancelQueries({ queryKey });
+
+        // TODO: add sessionKeys to sessionApi
+        const user = queryClient.getQueryData<UserDto>(['currentUser']);
+
+        if (!user) return undefined;
+
+        const { token, ...other } = user;
+        const author: ProfileDto = { ...other, following: false };
+
+        const prevComments =
+          queryClient.getQueryData<CommentDto[]>(queryKey) || [];
+
+        const newComment: CommentDto = {
+          // @ts-expect-error
+          id: slug,
+          createdAt: Date.now().toString(),
+          updatedAt: Date.now().toString(),
+          author,
+          body: comment.body,
+        };
+
+        const newComments: CommentDto[] = [...prevComments, newComment];
+
+        queryClient.setQueryData<CommentDto[]>(queryKey, newComments);
+
+        return { prevComments };
+      },
+
+      onError: (_, __, context) => {
+        queryClient.setQueryData(queryKey, context?.prevComments);
+      },
+
+      onSettled: () => {
+        queryClient.invalidateQueries({ queryKey });
+      },
+    },
+  );
+}
