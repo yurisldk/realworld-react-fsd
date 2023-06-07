3665da8dfe831e4ab3835ee585435ef5c483bb41
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-07 17:59:08 +0300

    test(entities/article): add ArticlesList test cases

---
+++ src/entities/article/ui/articles-list/ArticlesList.test.tsx
@@ -0,0 +1,91 @@
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import { ArticlesList } from './ArticlesList';
+
+const props = {
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  error: null,
+  hasNextPage: false,
+  infinityArticles: undefined,
+  renderArticles: vi.fn(),
+  nextPageAction: null,
+};
+
+describe('<ArticlesList />', () => {
+  it('renders loading state when isLoading is true', () => {
+    const loadingProps = {
+      ...props,
+      isLoading: true,
+    };
+
+    render(<ArticlesList {...loadingProps} />);
+    expect(screen.getByText('Loading articles...')).toBeInTheDocument();
+  });
+
+  it('renders error state when isError is true', () => {
+    const errorData = {
+      error: {
+        message: 'Error message',
+      },
+    };
+
+    const errorProps = {
+      ...props,
+      isError: true,
+      error: errorData,
+    };
+
+    // @ts-expect-error error is not assignable to type 'GenericErrorModel'
+    render(<ArticlesList {...errorProps} />);
+    expect(screen.getByText('Error message')).toBeInTheDocument();
+  });
+
+  it('renders empty state when isSuccess is true and no articles are available', () => {
+    const successEmptyProps = {
+      ...props,
+      isSuccess: true,
+      infinityArticles: { pages: [[]], pageParams: [undefined] },
+    };
+
+    render(<ArticlesList {...successEmptyProps} />);
+    expect(
+      screen.getByText('No articles are here... yet.'),
+    ).toBeInTheDocument();
+  });
+
+  it('renders articles when isSuccess is true and articles are available', () => {
+    const articles = [
+      { id: 1, title: 'Article 1' },
+      { id: 2, title: 'Article 2' },
+    ];
+
+    const successProps = {
+      ...props,
+      isSuccess: true,
+      infinityArticles: { pages: [articles], pageParams: [undefined] },
+      renderArticles: (article: { id: number; title: string }) => (
+        <div key={article.id}>{article.title}</div>
+      ),
+    };
+
+    // @ts-expect-error articles is not assignable to type 'ArticleDto[]'
+    render(<ArticlesList {...successProps} />);
+    expect(screen.getByText('Article 1')).toBeInTheDocument();
+    expect(screen.getByText('Article 2')).toBeInTheDocument();
+  });
+
+  it('renders next page action when hasNextPage is true', () => {
+    const successProps = {
+      ...props,
+      isSuccess: true,
+      hasNextPage: true,
+      infinityArticles: { pages: [[]], pageParams: [undefined] },
+      nextPageAction: <button type="button">Load More</button>,
+    };
+
+    render(<ArticlesList {...successProps} />);
+    expect(screen.getByText('Load More')).toBeInTheDocument();
+  });
+});
