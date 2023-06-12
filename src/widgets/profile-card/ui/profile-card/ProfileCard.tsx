0a486252a937d3ff4209e1ff92a49b1016362aeb
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-12 23:02:38 +0300

    refactor(widgets/profile-card):  update loading and error logic

--- src/widgets/profile-card/ui/profile-card/ProfileCard.tsx
+++ src/widgets/profile-card/ui/profile-card/ProfileCard.tsx
@@ -1,9 +1,35 @@
+import { ReactNode } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { profileApi } from '~entities/profile';
 import { sessionModel } from '~entities/session';
 import { ToggleFollowButton } from '~features/profile';
 import { PATH_PAGE } from '~shared/lib/react-router';
+import { ErrorHandler } from '~shared/ui/error-handler';
+import { Spinner } from '~shared/ui/spinner';
+
+type ProfileWrapperProps = {
+  children: ReactNode;
+};
+
+function ProfileWrapper(props: ProfileWrapperProps) {
+  const { children } = props;
+  return (
+    <div className="col-xs-12 col-md-10 offset-md-1">
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '11rem',
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
 
 type ProfileCardProps = {
   username: string;
@@ -25,6 +51,7 @@ export function ProfileCard(props: ProfileCardProps) {
     data: profile,
     isLoading,
     isError,
+    error,
     isSuccess,
   } = profileApi.useProfile(
     username,
@@ -32,14 +59,27 @@ export function ProfileCard(props: ProfileCardProps) {
     { enabled: !isCurrentUser },
   );
 
+  if (isError && error.status === 404)
+    return <Navigate to={PATH_PAGE.page404} />;
+
   const queryKey = profileApi.profileKeys.profile.username(username);
 
   return (
     <div className="user-info">
       <div className="container">
         <div className="row">
-          {isLoading && <div>loading</div>}
-          {isError && <div>error</div>}
+          {isLoading && (
+            <ProfileWrapper>
+              <Spinner />
+            </ProfileWrapper>
+          )}
+
+          {isError && (
+            <ProfileWrapper>
+              <ErrorHandler errorData={error} />
+            </ProfileWrapper>
+          )}
+
           {isSuccess && (
             <div className="col-xs-12 col-md-10 offset-md-1">
               <img
