c59e92a1063a7c5d8bcd679dff703fbfa99f21ff
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-19 12:52:58 +0300

    feat: setup vite font plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import unfonts from 'unplugin-fonts/vite';
 import checker from 'vite-plugin-checker';
 import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), checker({ typescript: true })],
+  plugins: [
+    react(),
+    eslint(),
+    checker({ typescript: true }),
+    unfonts({
+      google: {
+        families: [
+          { name: 'Titillium+Web', styles: 'wght@700' },
+          { name: 'Source+Serif+Pro', styles: 'wght@400;700' },
+          { name: 'Merriweather+Sans', styles: 'wght@400;700' },
+          {
+            name: 'Source+Sans+Pro',
+            styles: 'ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700',
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
