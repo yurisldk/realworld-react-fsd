efe8d90866f36a390f32533c18b5b082418574b8
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-23 16:45:33 +0300

    test(article/favorite): add favoriteArticle handler

---
+++ src/features/article/favorite/favorite-article/api/msw/postFavoriteArticleHandlers.ts
@@ -0,0 +1,81 @@
+import { rest } from 'msw';
+import { realworldApi } from '~shared/api/realworld';
+import {
+  server,
+  initTestDatabase,
+  parseTokenFromRequest,
+  mapMswArticleDto,
+} from '~shared/lib/msw';
+
+const databaseApi = initTestDatabase();
+
+const postFavoriteArticleHandlers = [
+  rest.post(
+    `${realworldApi.baseUrl}/articles/:slug/favorite`,
+    async (req, res, ctx) => {
+      const { slug } = req.params;
+
+      const token = parseTokenFromRequest(req);
+      const maybeUser = databaseApi.user.findFirst({
+        where: { token: { equals: token } },
+      });
+
+      if (!maybeUser)
+        return res(
+          ctx.status(401),
+          ctx.json({
+            status: 'error',
+            message: 'missing authorization credentials',
+          }),
+        );
+
+      const maybeArticle = databaseApi.article.findFirst({
+        where: { slug: { equals: String(slug) } },
+      });
+
+      if (!maybeArticle)
+        return res(
+          ctx.status(404),
+          ctx.json({ errors: { article: ['not found'] } }),
+        );
+
+      try {
+        const mswArticle = databaseApi.article.update({
+          where: { slug: { equals: String(slug) } },
+          data: {
+            ...maybeArticle,
+            favoritedBy: [...maybeArticle.favoritedBy, maybeUser.username],
+          },
+        });
+        const maybeProfile = databaseApi.profile.findFirst({
+          where: { username: { equals: maybeArticle.authorId } },
+        });
+
+        const articleDto = mapMswArticleDto(
+          mswArticle,
+          maybeUser,
+          maybeProfile,
+        );
+
+        return await res(
+          ctx.delay(1500),
+          ctx.status(200),
+          ctx.json({ article: articleDto }),
+        );
+      } catch (error) {
+        return await res(
+          ctx.status(422),
+          ctx.json({
+            errors: {
+              database: error,
+            },
+          }),
+        );
+      }
+    },
+  ),
+];
+
+export const setupPostFavoriteArticleHandlers = () => {
+  server.use(...postFavoriteArticleHandlers);
+};
