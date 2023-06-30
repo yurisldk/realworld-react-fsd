d4e6ad9fc46b52a4b9029472abdf3de814f29d72
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-30 15:45:24 +0300

    feat(app/providers): add ErrorBoundary

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,14 +1,18 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter } from 'react-router-dom';
+import { FullPageError } from '~shared/ui/full-page-error';
 import { QueryClientProvider } from './QueryClientProvider';
 import { Router } from './RouterProvider';
 import '~shared/main.css';
 
 export function Provider() {
   return (
-    <QueryClientProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={FullPageError}>
+      <QueryClientProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
