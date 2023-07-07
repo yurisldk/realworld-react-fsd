e1b28117fdd9231be8b193597c8214f1d7c8a892
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-07-07 15:47:28 +0300

    feat(entities/session): add guest guard component

--- src/entities/session/index.ts
+++ src/entities/session/index.ts
@@ -1,2 +1,4 @@
 export * as sessionApi from './api/sessionApi';
 export * as sessionModel from './model/sessionModel';
+export { AuthGuard } from './ui/auth-guard/AuthGuard';
+export { GuestGuard } from './ui/guest-guard/GuestGuard';
