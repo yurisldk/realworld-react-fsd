3dcbf12ec2e9e9f7293c59635e5af85e9a23c2f2
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-07-09 21:24:40 +0300

    refactor(pages/home): update tabs logic with shared/tabs

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,5 @@
-import { Tabs, TabsProps, useTabs } from '~shared/ui/tabs';
+import { useState } from 'react';
+import { Tabs } from '~shared/ui/tabs';
 import { GlobalArticlesList } from '~widgets/global-articles-list';
 import { PopularTags } from '~widgets/popular-tags';
 import { UserArticlesList } from '~widgets/user-articles-list';
@@ -7,53 +8,21 @@ type HomePageProps = {
   auth?: boolean;
 };
 
-enum TabKey {
-  Userfeed = 'userfeed',
-  Globalfeed = 'globalfeed',
-  Tagfeed = 'tagfeed',
-}
-
 export function HomePage(props: HomePageProps) {
   const { auth } = props;
 
-  const {
-    tabsState,
-    activeTab,
-    removeTab,
-    activateTab,
-    addTab,
-    deactivateAllTabs,
-  } = useTabs({
-    initialItems: [
-      {
-        key: TabKey.Userfeed,
-        label: `#${TabKey.Userfeed}`,
-        active: auth,
-        hidden: !auth,
-      },
-      {
-        key: TabKey.Globalfeed,
-        label: `#${TabKey.Globalfeed}`,
-        active: !auth,
-        hidden: false,
-      },
-    ],
-  });
+  const [activeTab, setActiveTab] = useState(auth ? 'userfeed' : 'globalfeed');
 
-  const onTabfeedClick = (tag: string) => {
-    deactivateAllTabs();
-    removeTab(TabKey.Tagfeed);
-    addTab({
-      key: TabKey.Tagfeed,
-      label: `#${tag}`,
-      active: true,
-      hidden: false,
-    });
+  const [tag, setTag] = useState<string | null>(null);
+
+  const handleTabChange = (value: string) => {
+    if (value !== 'tagfeed') setTag(null);
+    setActiveTab(value);
   };
 
-  const handleTabsChange: TabsProps['onChange'] = (selectedTab) => {
-    removeTab(TabKey.Tagfeed);
-    activateTab(selectedTab.key);
+  const handleTagClick = (_tag: string) => {
+    setTag(_tag);
+    setActiveTab('tagfeed');
   };
 
   return (
@@ -68,27 +37,33 @@ export function HomePage(props: HomePageProps) {
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
-            <div className="feed-toggle">
-              <Tabs items={tabsState} onChange={handleTabsChange} />
-            </div>
+            <Tabs keepUnmounted value={activeTab} onTabChange={handleTabChange}>
+              <div className="feed-toggle">
+                <Tabs.List>
+                  {auth && <Tabs.Tab value="userfeed">Your Feed</Tabs.Tab>}
+                  <Tabs.Tab value="globalfeed">Global Feed</Tabs.Tab>
+                  {tag && <Tabs.Tab value="tagfeed">#{tag}</Tabs.Tab>}
+                </Tabs.List>
+              </div>
 
-            {activeTab?.key === TabKey.Userfeed && (
-              <UserArticlesList query={{ limit: 10, offset: 0 }} />
-            )}
+              <Tabs.Panel value="userfeed">
+                <UserArticlesList query={{ limit: 10, offset: 0 }} />
+              </Tabs.Panel>
 
-            {activeTab?.key === TabKey.Globalfeed && (
-              <GlobalArticlesList query={{ limit: 10, offset: 0 }} />
-            )}
+              <Tabs.Panel value="globalfeed">
+                <GlobalArticlesList query={{ limit: 10, offset: 0 }} />
+              </Tabs.Panel>
 
-            {activeTab?.key === TabKey.Tagfeed && (
-              <GlobalArticlesList
-                query={{ limit: 10, offset: 0, tag: activeTab.key }}
-              />
-            )}
+              <Tabs.Panel value="tagfeed">
+                <GlobalArticlesList
+                  query={{ limit: 10, offset: 0, tag: tag! }}
+                />
+              </Tabs.Panel>
+            </Tabs>
           </div>
 
           <div className="col-md-3">
-            <PopularTags onTagClick={onTabfeedClick} />
+            <PopularTags onTagClick={handleTagClick} />
           </div>
         </div>
       </div>
