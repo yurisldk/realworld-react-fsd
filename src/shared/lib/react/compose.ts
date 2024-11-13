d2e68eb62667ef2d4e20474044d139e6c3a71cd6
Yuri Soldatenko <soldatenko.yuri@gmail.com>
2024-11-13 12:36:17 +0300

    feat: add forwardRef support

--- src/shared/lib/react/compose.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentType } from 'react'
-
-export function compose<Props extends object>(
-  ...hocs: Array<(component: ComponentType<Props>) => ComponentType<Props>>
-): (component: ComponentType<Props>) => ComponentType<Props> {
-  return (component: ComponentType<Props>) =>
-    hocs.reduceRight((wrapped, hoc) => hoc(wrapped), component)
-}
